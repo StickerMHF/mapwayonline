79fd926b0270404ed45955d0b469427f5d167ef5
MaoHaifeng <mhfflying@163.com>
2017-07-31 13:26:52 +0800

    0.1

--- src/components/formDesign/ContentLeft.vue
+++ src/components/formDesign/ContentLeft.vue
@@ -2,6 +2,9 @@
 	<div id="content-left">
 	<div class="cl_head">
 	<h2>表单</h2>
+	<div class="cl_header_menu dropdown">
+		<i class="el-icon-plus "></i>
+	</div>
 	</div>
 	</div>
 </template>
@@ -41,6 +44,9 @@
 .cl_head{
 	padding: 23px 18px 22px 18px;
     line-height: 22px;
+    position: absolute;
+    right: 0;
+    left: 0;
 }
 .cl_head h2{
 	float: left;
@@ -48,5 +54,21 @@
     margin-right: 10px;
     font-weight: bold;
     color: #333;
+    margin: 0;
+    padding: 0;
+}
+.cl_header_menu {
+    float: right;
+    position: relative;
+    height: 16px;
+    line-height: 16px;
+}
+.cl_header_menu i{
+	font-size: 12px;
+    line-height: 22px;
+    color: #2093ef;
+}
+.cl_header_menu i:hover{
+	cursor: pointer;
 }
 </style>
